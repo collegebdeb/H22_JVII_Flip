48b4be53e1ebccca5f1301baccb6b5a0486fc200
ZackLefebvreBDEB <2041601@bdeb.qc.ca>
2022-03-14 17:54:15 -0400

    Ajout d'animations, un peu de coding pour le mouvement, et des assets.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // la vitesse que le joueur va avoir lorsqu'il va bouger
+    public float vitesseMarche = 5f; 
+
+    // l'enregistrement physique du personnage et du joueur dans le monde
+    public Rigidbody2D rigidbody;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       // On va mettre du code ici afin de  
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
