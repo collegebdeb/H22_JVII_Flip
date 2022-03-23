c1501799bff731ce394d515c2e2952323b5d0421
Raphael taillefer <2050963@bdeb.qc.ca>
2022-03-23 14:16:56 -0400

    Movement Fixed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // la vitesse que le joueur va avoir lorsqu'il va bouger
-    public float vitesseMarche = 5f; 
+    public float speed = 5f;
+    float x;
+    float y;
 
     // l'enregistrement physique du personnage et du joueur dans le monde
     public Rigidbody2D rb;
 
     // Utilisé pour enregistrer le valeur du mouvement du joueur
     // puis l'utiliser dans notre FixedUpdate afin de programmer le physique du mouvement
-    Vector2 mouvementJoueur;
+    Vector2 move;
 
     // Ceci est pour assigner les animations de la chasseuse avec le mouvement du joueur
     public Animator animateurJoueur;
@@ -22,30 +24,29 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // Ici update sera utilisé afin de coder les inputs du joueur 
+        MoveInput(); // donner vitesse joueur
 
        // On cherche l'input du donnée horizontale ou verticale du joueur lorsqu'il bouge 
-       mouvementJoueur.x = Input.GetAxisRaw("Horizontal");
-       mouvementJoueur.y = Input.GetAxisRaw("Vertical");
+       x = Input.GetAxisRaw("Horizontal");
+       y = Input.GetAxisRaw("Vertical");
     
        // Ceci est l'assignement de l'animation selon l'axe de mouvement du personnage et selon si
        // le joueur bouge. Sinon, la chasseuse reste en animation Idle
-       animateurJoueur.SetFloat("mouvementHorizontale", mouvementJoueur.x); 
-       // Debug.Log(mouvementJoueur);
-       animateurJoueur.SetFloat("mouvementVerticale", mouvementJoueur.y); 
-       animateurJoueur.SetFloat("joueurVitesse", mouvementJoueur.sqrMagnitude);
+
+       animateurJoueur.SetFloat("mouvementHorizontale", move.x); 
+       animateurJoueur.SetFloat("mouvementVerticale", move.y); 
+       animateurJoueur.SetFloat("joueurVitesse",  move.sqrMagnitude);
 
     }
 
     void FixedUpdate()
     {
-       // Ici update sera utilisé afin de coder les physiques du mouvement du jeu puisqu'il est appelé 50 fois par
-       // frame faisant celui-ci plus fiable.
+        // Ici update sera utilisé afin de coder les physiques du mouvement du jeu puisqu'il est appelé 50 fois par
+        // frame faisant celui-ci plus fiable.
 
-       // Le rigidbody va bouger à la nouvelle position grâce à l'input du joueur
-       rb.MovePosition(rb.position + mouvementJoueur * vitesseMarche * Time.fixedDeltaTime);
-       
-       Flip(mouvementJoueur.x);
+        // Le rigidbody va bouger à la nouvelle position grâce à l'input du joueur
+        Move();
+       Flip(move.x);
     }
 
     // Ceci est pour changer la direction du personnage en idle lorsqu'il a finit de bouger
@@ -62,5 +63,17 @@ public class PlayerMovement : MonoBehaviour
 
            transform.localScale = theScale;
        } 
+    }
+
+    void MoveInput()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+        move = new Vector2(x, y).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(move.x * speed, move.y * speed);
     }
 }
--- Assets/Scripts/teleporters/TeleportManager.cs
+++ Assets/Scripts/teleporters/TeleportManager.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeleportManager : MonoBehaviour
 {
     public Transform[] TpPos = new Transform[6];
     public GameObject[] Teleporters = new GameObject[2];
+    public Text InteractTP;
+    public bool CanTp;
 
     // Start is called before the first frame update
     void Start()
     {
         FileTeleportation();
+        InteractTP = GameObject.Find("CanTeleport").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name.StartsWith("Teleporter"))
+        {
+            InteractTP.text = "Press E to teleport";
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.StartsWith("Teleporter"))
+        {
+            InteractTP.text = "";
+        }
         
     }
 
