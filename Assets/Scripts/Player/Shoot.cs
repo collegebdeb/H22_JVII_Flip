14dad5267e77dea0a1b88a79bb59de1ff0256b06
Raphael Taillefer <raphael.taillefer@gmail.com>
2022-03-31 08:31:00 -0400

    ShootOkay

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate2 : MonoBehaviour
+public class Shoot : MonoBehaviour
 {
     public float offset = 0.0f;
+    public GameObject arrow;
+    public Transform firepoint;
+
+
     // Use this for initialization
     void Start()
     {
@@ -14,9 +18,25 @@ public class Rotate2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Rotation();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootArrow();
+        }
+    }
+
+    void Rotation()
+    {
+        //Rotate towards mouse
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         difference.Normalize();
         float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset);
+    }
+
+    void ShootArrow()
+    {
+        Instantiate(arrow, firepoint.position, transform.rotation);
     }
 }
