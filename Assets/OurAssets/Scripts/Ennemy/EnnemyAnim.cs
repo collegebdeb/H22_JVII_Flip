afe61ae37a64331681b1e814dcf61798088d237a
Raphael Taillefer <raphael.taillefer@gmail.com>
2022-04-03 21:23:04 -0400

    EnnemyMove Fix

--- Assets/OurAssets/Scripts/Ennemy/EnnemyAnim.cs
+++ Assets/OurAssets/Scripts/Ennemy/EnnemyAnim.cs
@@ -11,41 +11,62 @@ public class EnnemyAnim : MonoBehaviour
     public GameObject Player;
     public float range;
     public AIDestinationSetter SetDestination;
+    public AIPath path;
     public Transform Idleposition;
+
+    float currentposition;
+    float lastposition = 0;
 
     public enum State // différents states pour l'ennemi
     {
         Idle,
         Aggro,
         Chase,
+        Fire,
         Stop
     }
 
-
     public State state;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
-
-    void Chase()
-    {
-        state = State.Chase;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        ChangeState();
-        //Aggro();
+        ChangeState();   //Changer le state
+        CheckPosition(); //Pivoter le personnage 
+        Alert();         //changer l'alert si le personnage est alerted ou nons
+    }
+
+    void Chase() //partir la chasse quand le joueur est assez proche, fonction appelée dans l'animation
+    {
+        state = State.Chase;
+        animator.SetBool("ReachedPos", false);
+    }
+
+    void AfterCharge() //Une fois que le robot a finit de charger et de tirer, regarder ou se trouve le joueur pour déterminer son état
+    {
+        animator.SetBool("Fire", false);
+
+        if (Vector3.Distance(transform.position, GameManager.Instance.Player.position) == 0)
+        {
+            Alerted = false;
+            state = State.Idle;
+        }
+        else
+        {
+            state = State.Chase;
+        }
+    }
+    void CheckPosition()
+    {
+        currentposition = transform.position.x;
+        animator.SetFloat("Horizontal", currentposition - lastposition);
+        lastposition = currentposition;
 
 
     }
+    // Update is called once per frame
 
-    void Alert() // gère les animations de l'ennemi de Idle à Alert
+
+    void Alert() // gère les animations de l'ennemi de Idle à Alert et son Aggro
     {
         if (Alerted)
         {
@@ -60,51 +81,58 @@ public class EnnemyAnim : MonoBehaviour
     }
 
 
-    void ChangeState() // Change l'état de l'ennemi
+    /* if (Vector3.Distance(transform.position, GameManager.Instance.Player.position) >= range)
+        {
+            Alerted = false;
+            aggro = false;
+            state = State.Idle;
+            animator.SetBool("Fire", false);
+         
+        }*/
+
+
+
+void ChangeState() // Change l'état de l'ennemi
     {
         switch (state)
         {
-            case State.Aggro:
-                Alert();
-                if (Vector3.Distance(transform.position, GameManager.Instance.Player.position) >= range)
-                {
-                    Alerted = false;
-                    aggro = false;
-                    state = State.Idle;
-                    Alert();
-                }
-                break;
+          
 
             case State.Idle:
+                if (Vector3.Distance(transform.position, Idleposition.position) <= 1f)
+                {
+                    animator.SetBool("ReachedPos", true);
+                }
+                path.maxSpeed = 4;
+                SetDestination.target = Idleposition.transform;
+                Alerted = false;
                 if (!aggro && Vector3.Distance(transform.position, GameManager.Instance.Player.position) < range)
                 {
                     Alerted = true;
-                    state = State.Aggro;
                 }
                 break;
 
+        
             case State.Chase:
-                SetDestination.target = GameManager.Instance.Player;
-                if (!aggro && Vector3.Distance(transform.position, GameManager.Instance.Player.position) > range)
+                SetDestination.target = GameManager.Instance.Player;  //Player gets targeted
+                path.maxSpeed = 4;
+                if (!aggro && Vector3.Distance(transform.position, GameManager.Instance.Player.position) > range + 3f) //If player out of range
                 {
-                    print("lets go");
-                    state = State.Stop;
+                    print("Not aggro anymore");
+                    state = State.Idle;
+                }
+                if(Vector3.Distance(transform.position,GameManager.Instance.Player.position) <= 5)
+                {
+                    state = State.Fire;
                 }
                 break;
 
-            case State.Stop:
+            case State.Fire:
                 {
-                    SetDestination.target = Idleposition.transform;
-                    if (Vector3.Distance(transform.position,Idleposition.position) < 1)
-                    {
-                        Alerted = false;
-                        aggro = false;
-                        state = State.Idle;
-                        Alert();
-                    }
-                }
-              
-                break;
+                    animator.SetBool("Fire", true);
+                    path.maxSpeed = 0;
+                } break;
+
 
             default:
                 break;
