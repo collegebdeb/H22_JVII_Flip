0f0122ba10837bd2c911c1d123e611128474d6b6
Raphael taillefer <2050963@bdeb.qc.ca>
2022-04-04 17:49:09 -0400

    dash

--- Assets/OurAssets/Scripts/Player/PlayerMovement.cs
+++ Assets/OurAssets/Scripts/Player/PlayerMovement.cs
@@ -9,8 +9,12 @@ public class PlayerMovement : MonoBehaviour
     float x;
     float y;
 
+    public Transform dashtarget;
+
     // l'enregistrement physique du personnage et du joueur dans le monde
     public Rigidbody2D rb;
+
+    public GameObject child; //Rotation du viseur
 
     // Utilisé pour enregistrer le valeur du mouvement du joueur
     // puis l'utiliser dans notre FixedUpdate afin de programmer le physique du mouvement
@@ -18,12 +22,25 @@ public class PlayerMovement : MonoBehaviour
 
     // Ceci est pour assigner les animations de la chasseuse avec le mouvement du joueur
     public Animator animateurJoueur;
-
     public bool personnageDirectionDroit;
+
+
+    private void Start()
+    {
+        child = gameObject.transform.GetChild(0).gameObject;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Dash();
+            
+        }
+
+        DashtargetClamp();
+
         MoveInput(); // donner vitesse joueur
 
        // On cherche l'input du donnée horizontale ou verticale du joueur lorsqu'il bouge 
@@ -36,6 +53,14 @@ public class PlayerMovement : MonoBehaviour
        animateurJoueur.SetFloat("mouvementHorizontale", move.x); 
        animateurJoueur.SetFloat("mouvementVerticale", move.y); 
        animateurJoueur.SetFloat("joueurVitesse",  move.sqrMagnitude);
+
+    }
+
+    void Dash()
+    {
+
+        // rb.AddForce(child.transform.right * 4000, ForceMode2D.Force);
+        // rb.MovePosition(dashtarget.transform.position * 5f*Time.deltaTime);
 
     }
 
@@ -76,4 +101,27 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(move.x * speed, move.y * speed);
     }
+
+    void DashtargetClamp()
+    {
+        /*float clampx = 10f;
+        float clampy = 10f;
+        Vector3 Newposition;
+        Vector3 mouseposition;
+        Newposition = transform.position;
+        mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Newposition.x = Mathf.Clamp(mouseposition.x, -clampx, clampx);
+        Newposition.y = Mathf.Clamp(mouseposition.y, -clampy, clampy);
+        dashtarget.position = Newposition;*/
+        Vector3 initialpos = transform.position;
+        Vector3 mouseposition;
+
+        mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var allowedPos = mouseposition - initialpos;
+        allowedPos = Vector3.ClampMagnitude(allowedPos, 8.0f);
+        dashtarget.position = initialpos + allowedPos;
+
+    }
+
+ 
 }
