90400e35ea08724a9cfceac4eb4caf54f79575a3
remimorneaubdeb <2060097@bdeb.qc.ca>
2022-04-14 17:39:34 -0400

    dash script

---
+++ Assets/OurAssets/Scripts/Player/Dashparticle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dashparticle : MonoBehaviour
+{
+    public PlayerMovement dash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dash = gameObject.GetComponent<PlayerMovement>();
+        dash.isdashing
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
