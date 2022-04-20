ab764819fda155387368647a359cad7f77291465
remimorneaubdeb <2060097@bdeb.qc.ca>
2022-04-20 14:46:12 -0400

    ajustement du code

--- Assets/OurAssets/Scripts/Player/Dashparticle.cs
+++ Assets/OurAssets/Scripts/Player/Dashparticle.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Dashparticle : MonoBehaviour
 {
+  // PlayerController player;
     public PlayerMovement dash;
+    public GameObject ghostPrefab;
+    public float delay = 1.0f;
+    float delta = 0;
+    SpriteRenderer spriteRenderer;
+    public float destroyTime = 0.1f;
+    public Color color;
+    public Material materiel =null;
+
     // Start is called before the first frame update
     void Start()
     {
-        //    dash = gameObject.GetComponent<PlayerMovement>();
-        //    dash.isdashing
+         dash = gameObject.GetComponent<PlayerMovement>();
+        if (dash.isdashing)
+        {
+
+        }
         // }
 
         // Update is called once per frame
@@ -18,4 +30,13 @@ public class Dashparticle : MonoBehaviour
 
         //}
     }
+    void createGhost()
+    {
+        GameObject ghostObj = Instantiate(ghostPrefab, transform.position, transform.rotation);
+        //ghostObj.transform.localScale = player.transform.localScale;
+        Destroy(ghostObj, destroyTime);
+        spriteRenderer = ghostObj.GetComponent<SpriteRenderer>();
+        //spriteRenderer.sprite = player;
+    }
 }
+
