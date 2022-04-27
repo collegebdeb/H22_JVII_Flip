fd9c1e25c4112b7a520ea32c32273f6033b611a6
remimorneaubdeb <2060097@bdeb.qc.ca>
2022-04-27 14:49:23 -0400

    code afterimage

--- Assets/OurAssets/Scripts/Player/AfterImage_Sprite.cs
+++ Assets/OurAssets/Scripts/Player/AfterImage_Sprite.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class AfterImage_Sprite : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float activeTime = 0.1f;
+    private float timeActivated;
+    private float alpha;
+    [SerializeField]
+    private float alphaset = 0.8f;
+    private float alphamultiplier = 0.85f;
+    private Transform player;
+    private SpriteRenderer SR;
+    private SpriteRenderer playerSR;
+    private Color color;
 
-    // Update is called once per frame
-    void Update()
+
+
+    // Start is called before the first frame update
+    private void OnEnable()
     {
-        
+        SR = GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerSR = player.GetComponent<SpriteRenderer>();
+
+        alpha = alphaset;
+        SR.sprite = playerSR.sprite;
+        transform.position = player.position;
+        transform.rotation = player.rotation;
+        timeActivated = Time.time;
+    }
+    private void Update()
+    {
+        alpha *= alphamultiplier;
+        color = new Color(1f, 1f, 1f, alpha);
+        SR.color = color;
+
+        if (Time.time >= (timeActivated + activeTime))
+        {
+            //Add back to pool.
+        }
     }
 }
